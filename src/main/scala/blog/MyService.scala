00d684f646cdcdb3d4932480016cd56156227b4f
Zhranklin <zhranklin@outlook.com>
2016-04-08 15:58:25 +0800

    对/editor/submit部分的路由进行了重构,代码更优雅

--- src/main/scala/blog/MyService.scala
+++ src/main/scala/blog/MyService.scala
@@ -1,13 +1,16 @@
 package com.zhranklin.blog
 
+import java.util.Date
+
 import akka.actor.Actor
 import com.mongodb.casbah.Imports._
-import com.mongodb.casbah.Imports.{MongoDBObject => $, MongoDBList => $$}
+import com.mongodb.casbah.Imports.{MongoDBList => $$, MongoDBObject => $}
 import spray.httpx.PlayTwirlSupport._
 import spray.routing._
 import org.bson.types.ObjectId
-
 import db._
+
+import scala.util.Try
 
 class MyServiceActor extends Actor with MyService {
   def actorRefFactory = context
@@ -26,7 +29,7 @@ trait MyService extends HttpService {
     path("refresh") {
       complete {
         refreshArticleList()
-        "successfully refreshed"
+        html.message.render("Info", "刷新成功.")
       }
     } ~
     path("blog" / Rest) {str =>
@@ -40,18 +43,24 @@ trait MyService extends HttpService {
       post {
         anyParams('id, 'title, 'html, 'markdown, 'tags) { (id, title, Hhtml, markdown, tags) =>
           complete {
+            val (createTime, bid) = {
+              val timeAndId = for {
+                bid ← Try(new ObjectId(id)).toOption //验证id这个字符串是否符合ObjectId构造器的要求,如果不符合则可认为是新文章
+                mongo ← articles.findOneByID(bid)
+                createTime ← mongo.getAs[Date]("create_time")
+              } yield (Some(createTime), Some(bid))
+              timeAndId.getOrElse(None, None)
+            }
             val art = Article(
               title = title.trim.dropWhile(_ == '#').trim,
-              author = "Zhranklin",
-              mdown = Some(markdown),
-              html = Hhtml,
+              author = "Zhranklin", mdown = Some(markdown), html = Hhtml,
+              create_time = createTime.getOrElse(new Date),
               abs = Hhtml.replaceAll("<.*?>", " ").replaceAll("\\s\\s+", " ").take(200),
-              tags = ",".r.split(tags).map(_.trim).toList.filterNot(_ matches "\\s*")
-            ).mongo
-            if (id.size > 5)
-              articles.update("_id" $eq new ObjectId(id.trim), art)
+              tags = ",".r.split(tags).map(_.trim).toList.filterNot(_ matches "\\s*"))
+            if (bid.nonEmpty)
+              articles.update("_id" $eq bid.get, art.mongo)
             else
-              articles.insert(art)
+              articles.insert(art.mongo)
             refreshArticleList()
             html.message.render("Info", "修改/添加成功.")
           }
